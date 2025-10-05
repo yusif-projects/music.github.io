aa775fe3d240a0adf79101824bb1652442667526
Yusif Aliyev <yusif.projects@gmail.com>
2025-10-05 15:15:16 +0400

    bugfix

--- js/main.js
+++ js/main.js
@@ -238,7 +238,7 @@ function renderReleases(data) {
         <div class="card-body d-flex flex-column">
           <div class="d-flex justify-content-between align-items-center mb-2">
             <h5 class="card-title mb-0">${r.title}</h5>
-            <span class="chip">${r.type} • ${r.year}</span>
+            <span class="chip">${r.year}</span>
           </div>
           <div class="mt-auto d-flex flex-wrap gap-2">
             ${ytBtn}
